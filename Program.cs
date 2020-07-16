93aa494d3ea87a57ab47645506ab45040021cfb7
vikaBu <vikabuklajevafoto@gmail.com>
2020-07-16 09:44:04 +0100

    working stage 1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 
 namespace Bus_API
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello World!");
+            var transportApiClient = new TransportApiClient();
+
+            var stopCode = GetStopCodeFromUser();
+
+            var departures = transportApiClient.GetBusDeparturesForStop(stopCode);
+            
+            PrintNextBuses(departures); 
+
+
         }
+        private static void PrintNextBuses(BusDepartureResponse departures)
+        {
+            Console.WriteLine($"\n\nNext buses at {departures.Name}");
+            foreach (var departure in departures.Departures.All.Take(5))
+            {
+                Console.WriteLine($"Line: {departure.Line}, To: {departure.Direction}, Leaving at: {departure.ExpectedDepartureTime}");
+            }
+
+        }
+            private static string GetStopCodeFromUser()
+            {
+                Console.WriteLine("Please enter the bus stop code:");
+                return Console.ReadLine();
+            }
+            
     }
 }
